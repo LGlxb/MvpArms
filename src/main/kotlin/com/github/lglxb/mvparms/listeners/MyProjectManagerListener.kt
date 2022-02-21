9918b25280632a2c77ca18849f6d93ca50c548e1
GitHub Action <action@github.com>
2022-02-21 06:07:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/lglxb/mvparms/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/lglxb/mvparms/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.lglxb.mvparms.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.lglxb.mvparms.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/test/kotlin/com/github/lglxb/mvparms/MyPluginTest.kt
+++ src/test/kotlin/com/github/lglxb/mvparms/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.lglxb.mvparms
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
